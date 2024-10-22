c484c63a93249256b0b9c5a7be4b0f587e43b1d4
Mateusz Ciszczoń <contact@mciszczon.pl>
2024-10-22 14:34:00 +0200

    Update generic pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,54 +1,52 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://onhive.io',
-  integrations: [starlight({
-    title: 'OnHive.io',
-    logo: {
-      src: './src/assets/hive.svg'
-    },
-    social: {
-      email: 'mailto:info@onhive.io',
-      github: 'https://github.com/instytutfi'
-    },
-    sidebar: [{
-      label: '[home] Home',
-      link: '/'
-    }, {
-      label: '[list] Features',
-      link: '/features/'
-    }, {
-      label: '[box] Guides',
-      autogenerate: {
-        directory: 'guides'
-      }
-    }, {
-      label: '[book] Reference',
-      autogenerate: {
-        directory: 'reference'
-      }
-    }],
-    components: {
-      ThemeProvider: './src/components/ThemeProvider.astro',
-      ThemeSelect: './src/components/ThemeSelect.astro',
-      SiteTitle: './src/components/SiteTitle.astro',
-      Sidebar: './src/components/Sidebar.astro',
-      Pagination: './src/components/Pagination.astro',
-      Hero: './src/components/Hero.astro',
-      Head: './src/components/Head.astro',
-      PageTitle: './src/components/PageTitle.astro'
-    },
-    customCss: [
-      './src/styles/theme.css'
-    ],
-    expressiveCode: {
-      themes: ['github-dark']
-    },
-    pagination: false,
-    lastUpdated: true
-  })],
+  site: "https://onhive.io",
+  integrations: [
+    starlight({
+      title: "OnHive.io",
+      logo: {
+        src: "./src/assets/hive.svg"
+      },
+      social: {
+        github: "https://github.com/instytutfi/onhive.io"
+      },
+      sidebar: [
+        {
+          label: "[home] Home",
+          link: "/"
+        },
+        {
+          label: "[rocket] Get Started",
+          link: "/get-started/"
+        },
+        {
+          label: "[book] Hive Coding School",
+          autogenerate: {
+            directory: "school"
+          }
+        }
+      ],
+      components: {
+        ThemeProvider: "./src/components/ThemeProvider.astro",
+        ThemeSelect: "./src/components/ThemeSelect.astro",
+        SiteTitle: "./src/components/SiteTitle.astro",
+        Sidebar: "./src/components/Sidebar.astro",
+        Pagination: "./src/components/Pagination.astro",
+        Hero: "./src/components/Hero.astro",
+        Head: "./src/components/Head.astro",
+        PageTitle: "./src/components/PageTitle.astro"
+      },
+      customCss: ["./src/styles/theme.css"],
+      expressiveCode: {
+        themes: ["github-dark"]
+      },
+      pagination: false,
+      lastUpdated: true
+    })
+  ],
   output: "static"
 });
---
+++ src/content/docs/school/index.md
@@ -0,0 +1,6 @@
+---
+title: What is Hive Coding School?
+description: Test
+---
+
+Test
